f7c76329fbef666f8bcc0ad188f8726e4a84c970
mskcrypto13 <102144506+mskcrypto13@users.noreply.github.com>
2022-03-22 11:42:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+in
+main
+<> Edit new file
+Preview
+function Block(letter, rate, used, father, code) {
+this.letter letter; =
+this.rate= rate;
+this.used = used;
+this. father father;
+this.code = code;
+}
+function FindCode (block) {
